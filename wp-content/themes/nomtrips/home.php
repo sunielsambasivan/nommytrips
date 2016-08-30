595bb00ef443a10ac2741eafeee1e894855a9a13
Suniel Sambasivan <dravidian7@yahoo.com>
2016-08-29 23:14:58 -0600

    added footer

--- wp-content/themes/nomtrips/home.php
+++ wp-content/themes/nomtrips/home.php
@@ -4,164 +4,160 @@
 if ( !defined('ABSPATH')) exit;
 
 /**
- * Homepage Template
- * Template Name: home
- *
- * @file           home.php
- * @package        NomTrips 
- * @author         Suniel Sambasivan
- * @copyright      2015 - 2016 Suniel Sambasivan
- */
+* Homepage Template
+* Template Name: home
+*
+* @file           home.php
+* @package        NomTrips 
+* @author         Suniel Sambasivan
+* @copyright      2015 - 2016 Suniel Sambasivan
+*/
 
 get_header(); ?>
 
-<div id="content">
-  <section id="banner" class="banner picturefill-background">
-    <div class="banner--inner">
-      <?php 
-      //nt_debug($id); 
-        if ( has_post_thumbnail()) :
-          get_template_part(NT_COMPONENTS_PATH . 'layout/banner');
-        endif; 
-      ?>
-    
-      <div class="form-search--cities">
-      <?php
-        get_template_part(NT_COMPONENTS_PATH . 'forms/search', '-cities');
-      ?>
-      </div>
+<section id="banner" class="banner picturefill-background">
+  <div class="banner--inner">
+    <?php 
+    //nt_debug($id); 
+      if ( has_post_thumbnail()) :
+        get_template_part(NT_COMPONENTS_PATH . 'layout/banner');
+      endif; 
+    ?>
+  
+    <div class="form-search--cities">
+    <?php
+      get_template_part(NT_COMPONENTS_PATH . 'forms/search', '-cities');
+    ?>
     </div>
-  </section>
+  </div>
+</section>
+      
+<?php if (have_posts()) : ?>
+  <?php while (have_posts()) : the_post(); ?>
+  <div class="row">
+    <div class="content columns small-12" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+      <section class="content--heading">
+        <?php the_content(); ?>
+      </section>
+      <section class="content--post">
+        <div class="row">
+          <?php
+          /* get cities */
+          $city_array = array( 'new-york', 'los-angeles', 'san-diego', 'miami', 'las-vegas' ); //set order
+          $image_size = 'thumb-card';
         
-  <?php if (have_posts()) : ?>
-
-    <?php while (have_posts()) : the_post(); ?>
-    <div class="row">
-      <div class="content columns small-12" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-        <section class="content--heading">
-          <?php the_content(); ?>
-        </section>
-        <section class="content--post">
-          <div class="row">
-            <?php
-            /* get cities */
-            $city_array = array( 'new-york', 'los-angeles', 'san-diego', 'miami', 'las-vegas' ); //set order
-            $image_size = 'thumb-card';
-          
-              /* show cities */
-            foreach( $city_array as $city ) {
-              switch( $city ) {
-                
-                case 'new-york': 
-                $current = City::get_city_by_slug( $city );
-                $image_size = 'thumb-card';
-                ?>
-                  <article class="columns small-12 medium-6 primary-content">
-                    <?php
-                      include( locate_template( NT_COMPONENTS_PATH .'cards/card--city.php') );
-                    ?>  
-                  </article>
-                  <?php 
-                break;
-                
-                case 'los-angeles': 
-                $current = City::get_city_by_slug( $city );
-                $image_size = 'thumb-card';
-                ?>
-                  <article class="columns small-12 medium-6 primary-content">
-                    <?php
-                      include( locate_template( NT_COMPONENTS_PATH .'cards/card--city.php') );
-                    ?>  
-                  </article>
-                  <?php 
-                break;
-                
-                case 'san-diego': 
-                $current = City::get_city_by_slug( $city );
-                $image_size = 'thumb-card';
-                ?>
-                  <article class="columns small-12 medium-4">
-                    <?php
-                      include( locate_template( NT_COMPONENTS_PATH .'cards/card--city.php') );
-                    ?>  
-                  </article>
-                  <?php 
-                break;
-                
-                case 'miami': 
-                $current = City::get_city_by_slug( $city );
-                //$image_size = 'thumb-card-vert';
-                ?>
-                  <article class="columns small-12 medium-4">
-                    <?php
-                      include( locate_template( NT_COMPONENTS_PATH .'cards/card--city.php') );
-                    ?>  
-                  </article>
-                  <?php 
-                break;
-                
-                case 'las-vegas': 
-                $current = City::get_city_by_slug( $city );
-                $image_size = 'thumb-card';
-                ?>
-                  <article class="columns small-12 medium-4">
-                    <?php
-                      include( locate_template( NT_COMPONENTS_PATH .'cards/card--city.php') );
-                    ?>  
-                  </article>
-                  <?php 
-                break;
-                
-              }
+            /* show cities */
+          foreach( $city_array as $city ) {
+            switch( $city ) {
+              
+              case 'new-york': 
+              $current = City::get_city_by_slug( $city );
+              $image_size = 'thumb-card';
+              ?>
+                <article class="columns small-12 medium-6 primary-content">
+                  <?php
+                    include( locate_template( NT_COMPONENTS_PATH .'cards/card--city.php') );
+                  ?>  
+                </article>
+                <?php 
+              break;
+              
+              case 'los-angeles': 
+              $current = City::get_city_by_slug( $city );
+              $image_size = 'thumb-card';
+              ?>
+                <article class="columns small-12 medium-6 primary-content">
+                  <?php
+                    include( locate_template( NT_COMPONENTS_PATH .'cards/card--city.php') );
+                  ?>  
+                </article>
+                <?php 
+              break;
+              
+              case 'san-diego': 
+              $current = City::get_city_by_slug( $city );
+              $image_size = 'thumb-card';
+              ?>
+                <article class="columns small-12 medium-4">
+                  <?php
+                    include( locate_template( NT_COMPONENTS_PATH .'cards/card--city.php') );
+                  ?>  
+                </article>
+                <?php 
+              break;
+              
+              case 'miami': 
+              $current = City::get_city_by_slug( $city );
+              //$image_size = 'thumb-card-vert';
+              ?>
+                <article class="columns small-12 medium-4">
+                  <?php
+                    include( locate_template( NT_COMPONENTS_PATH .'cards/card--city.php') );
+                  ?>  
+                </article>
+                <?php 
+              break;
+              
+              case 'las-vegas': 
+              $current = City::get_city_by_slug( $city );
+              $image_size = 'thumb-card';
+              ?>
+                <article class="columns small-12 medium-4">
+                  <?php
+                    include( locate_template( NT_COMPONENTS_PATH .'cards/card--city.php') );
+                  ?>  
+                </article>
+                <?php 
+              break;
+              
             }
+          }
+          ?>
+        </div>
+      </section><!-- end of .post-entry -->
+    </div><!-- end of content columns small-12-->
+    
+    <div class="content columns small-12">
+      <header class="content--heading">
+        <h2>Feature Guides</h2>
+      </header>
+      
+      <div class="content-desc">
+        <p>Whatever irony tacos gochujang mixtape, church-key crucifix shoreditch.</p>
+      </div>
+      
+      <div class="content--post">
+        <div class="row">
+          <div class="columns small-12 medium-4">
+            <?php
+              include( locate_template( NT_COMPONENTS_PATH .'cards/card--guides-featured.php') );
             ?>
           </div>
-        </section><!-- end of .post-entry -->
-      </div><!-- end of content columns small-12-->
-      
-      <div class="content columns small-12">
-        <header class="content--heading">
-          <h2>Feature Guides</h2>
-        </header>
-        
-        <div class="content-desc">
-          <p>Whatever irony tacos gochujang mixtape, church-key crucifix shoreditch.</p>
-        </div>
-        
-        <div class="content--post">
-          <div class="row">
-            <div class="columns small-12 medium-4">
-              <?php
-                include( locate_template( NT_COMPONENTS_PATH .'cards/card--guides-featured.php') );
-              ?>
-            </div>
-            
-            <div class="columns small-12 medium-4">
-              <?php
-                include( locate_template( NT_COMPONENTS_PATH .'cards/card--guides-featured.php') );
-              ?>
-            </div>
-            
-            <div class="columns small-12 medium-4">
-              <?php
-                include( locate_template( NT_COMPONENTS_PATH .'cards/card--guides-featured.php') );
-              ?>
-            </div>
-          </div><!--end of row-->
-        </div><!--end content--post-->
-      </div><!-- end of content columns small-12-->
-    </div><!--end of row-->
-      
-    <?php comments_template( '', true ); 
-      
-    endwhile; 
+          
+          <div class="columns small-12 medium-4">
+            <?php
+              include( locate_template( NT_COMPONENTS_PATH .'cards/card--guides-featured.php') );
+            ?>
+          </div>
+          
+          <div class="columns small-12 medium-4">
+            <?php
+              include( locate_template( NT_COMPONENTS_PATH .'cards/card--guides-featured.php') );
+            ?>
+          </div>
+        </div><!--end of row-->
+      </div><!--end content--post-->
+    </div><!-- end of content columns small-12-->
+  </div><!--end of row-->
+    
+  <?php comments_template( '', true ); 
+    
+  endwhile; 
 
-  else : 
+else : 
 
-    echo 'no content'; 
+  echo 'no content'; 
 
-  endif; ?>  
-      
-</div><!-- end of #content -->
-
+endif; ?>  
+    
 <?php get_footer(); ?>
--- wp-content/themes/nomtrips/includes/functions/nt-menus.php
+++ wp-content/themes/nomtrips/includes/functions/nt-menus.php
@@ -5,6 +5,7 @@
 
 function nt_register_menus() {
   register_nav_menu('menu-login',__( 'Login Menu' ));
+  register_nav_menu('menu-footer',__( 'Footer Menu' ));
 }
 add_action( 'init', 'nt_register_menus' );
 
