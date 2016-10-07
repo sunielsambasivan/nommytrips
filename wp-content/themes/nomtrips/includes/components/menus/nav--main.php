1897fa4a3f35e7becd8a050ee244f4340f929cc8
Suniel <suniel.sambasivan@ama.ab.ca>
2016-10-07 06:35:44 -0600

    added logic to top menu

--- wp-content/themes/nomtrips/includes/components/menus/nav--main.php
+++ wp-content/themes/nomtrips/includes/components/menus/nav--main.php
@@ -1,17 +1,27 @@
-<nav id="navMainMenu" role="navigation" class="small-3 medium-9 columns nav--main">
-  <?php
-  $user = wp_get_current_user();
-  if( $user->ID ) {
-    include( locate_template( NT_COMPONENTS_PATH .'menus/nav--logged-in--medium.php') );
-  }
+<div  class="small-3 medium-9">
+  <div class="row">
+    <?php
+    $user = wp_get_current_user();
+    $is_front = !is_front_page() ? false : true;
 
-  else {
+    if(!$is_front) { ?>
+      <div class="column small-3">
+        <?php //search cities if not home page
+        get_template_part(NT_COMPONENTS_PATH . 'forms/search', '-off-canvas-menu');
+        ?>
+      </div>
+    <?php } ?>
 
-    //search cities if not home page
-    if(!is_front_page())
-      get_template_part(NT_COMPONENTS_PATH . 'forms/search', '-off-canvas-menu');
+    <nav id="navMainMenu" role="navigation" class="nav--main column<?php echo !$is_front ? ' small-9' : ' small-12' ?>">
+      <?php
+      if( $user->ID ) {
+        include( locate_template( NT_COMPONENTS_PATH .'menus/nav--logged-in--medium.php') );
+      }
 
-    wp_nav_menu( array( 'container_class' => 'menu-header', 'theme_location' => 'menu-login' ) );
-  }
-  ?>
+      else {
+        wp_nav_menu( array( 'container_class' => 'menu-header', 'theme_location' => 'menu-login' ) );
+      }
+      ?>
+    </nav>
+  </div><!--/row-->
 </div>
