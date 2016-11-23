451ccff412722f6c1cfda842c9faf8bb652dd14c
Suniel <suniel.sambasivan@ama.ab.ca>
2016-11-23 05:02:37 -0700

    added search button on focus

--- wp-content/themes/nomtrips/includes/components/forms/search--header.php
+++ wp-content/themes/nomtrips/includes/components/forms/search--header.php
@@ -5,9 +5,11 @@
 ?>
 
 <div class="search--cities--header">
-  <form id="search-cities">
+  <form id="search-cities" class="search--form">
     <div class="form-item">
-      <input type="search" name="city" class="input-city" placeholder="Where would you like to eat today?" />
+      <input type="search" name="city" class="search--input-text" placeholder="Where would you like to eat today?" />
+      <i class="search--icon fa fa-search"></i>
+      <button class="search--btn button fa fa-search"></button>
     </div>
   </form>
 </div>
